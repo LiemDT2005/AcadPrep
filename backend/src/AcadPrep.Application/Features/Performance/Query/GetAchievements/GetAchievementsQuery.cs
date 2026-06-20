6c63fc87deb07513fc18e95d0d7a870342117847
LiemDT2005 <liemdt.ce190697@gmail.com>
2026-06-20 10:48:37 +0700

    fix: conflict

--- backend/src/AcadPrep.Application/Features/Performance/Query/GetAchievements/GetAchievementsQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using AcadPrep.Application.Common.Models;
-using System.Collections.Generic;
-using AcadPrep.Application.Features.Performance.DTOs;
-using MediatR;
-
-namespace AcadPrep.Application.Features.Performance.Queries.GetAchievements;
-
-public record GetAchievementsQuery(int UserId) : IRequest<Result<List<AchievementDto>>>;
-
